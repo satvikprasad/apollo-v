43436b5def3cad4907f62dff0ef13cc4996254d7
Satvik Prasad <satvikprasad@gmail.com>
2023-12-29 19:50:20 +1100

    Add workflows

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+
+SET include=-Ilib\raylib\src
+SET linker=lib\raylib\src\libraylib.a -lgdi32 -lole32 -loleaut32 -limm32 -lwinmm
+
+mkdir build
+
+gcc src\state.c -o .\build\libstate.so -fPIC -shared %include% %linker%
+
+gcc src\main.c -Wall -Wextra -o build\lynx %include% %linker% -L.\build\
+
+mkdir -p build\assets
+
+cp -r .\assets\* build\assets
+
