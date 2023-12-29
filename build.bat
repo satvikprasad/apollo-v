bbffd6a698ba3f0debbdfd9bd9ce85878e635a25
Satvik Prasad <satvikprasad@gmail.com>
2023-12-30 10:52:46 +1100

    Fix build.bat

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ SET linker=lib\raylib\src\libraylib.a -lgdi32 -lole32 -loleaut32 -limm32 -lwinmm
 
 mkdir build
 
-gcc src\state.c -o .\build\libstate.so -fPIC -shared %include% %linker%
+REM gcc src\state.c -o .\build\libstate.so -fPIC -shared %include% %linker%
 
 gcc src\main.c src\state.c -Wall -Wextra -o build\lynx.exe %include% %linker% -L.\build\
 
