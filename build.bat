96e97b0a888ba27e6b28fc5cfe6c8419d4665316
Satvik Prasad <satvikprasad@gmail.com>
2023-12-29 19:58:25 +1100

    Change exe type

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ mkdir build
 
 gcc src\state.c -o .\build\libstate.so -fPIC -shared %include% %linker%
 
-gcc src\main.c -Wall -Wextra -o build\lynx %include% %linker% -L.\build\
+gcc src\main.c -Wall -Wextra -o build\lynx.exe %include% %linker% -L.\build\
 
 mkdir -p build\assets
 
