8823e36cef7bc7b301ff47a2a55eb9a233e1c863
Satvik Prasad <satvikprasad@gmail.com>
2024-01-27 11:35:00 +1100

    Windows lua

--- src/ffmpeg_windows.c
+++ src/ffmpeg_windows.c
@@ -4,8 +4,8 @@
 #include "ffmpeg.h"
 #include "handmademath.h"
 
-i32 FFMPEGStart(HMM_Vec2 size, u32 fps, const char *music) { return -1; }
+I32 FFMPEGStart(HMM_Vec2 size, U32 fps, const char *music) { return -1; }
 
-void FFMPEGEnd(i32 pipe) {}
+void FFMPEGEnd(I32 pipe) {}
 
-void FFMPEGWrite(i32 pipe, void *data, HMM_Vec2 size) {}
+void FFMPEGWrite(I32 pipe, void *data, HMM_Vec2 size) {}
