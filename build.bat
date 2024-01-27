b87f07cd1e498063adbe4f328f566d69929e20f6
Satvik Prasad <satvikprasad@gmail.com>
2024-01-27 11:35:00 +1100

    Windows lua

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
-SET include=-Ilib\raylib\src
-SET linker=lib\raylib\src\libraylib.a -lgdi32 -lole32 -loleaut32 -limm32 -lwinmm
+SET include=-Ilib\raylib\src -Ilib\lua-5.4.6\src
+SET linker=lib\raylib\src\libraylib.a lib\lua-5.4.6\src -lgdi32 -lole32 -loleaut32 -limm32 -lwinmm
 SET src=src\lmath.c src\main.c src\state.c src\ffmpeg_windows.c src\signals.c src\renderer.c src\parameter.c src\hashmap.c src\loopback.c src\api.c src\arena.c src\permanent_storage.c
 
 mkdir build
