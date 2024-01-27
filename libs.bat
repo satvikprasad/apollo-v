370741455d17eda81e815493b59768552eb27a78
Satvik Prasad <satvikprasad@gmail.com>
2024-01-27 11:35:00 +1100

    Windows lua

--- libs.bat
+++ libs.bat
@@ -3,3 +3,11 @@ cd lib\raylib\src
 make 
 
 cd ..\..\..\
+
+cd lib
+curl -L -R -O https://www.lua.org/ftp/lua-5.4.6.tar.gz
+tar zxf lua-5.4.6.tar.gz
+cd lua-5.4.6
+make all
+cd ../../
+rm lib/lua-5.4.6.tar.gz 
